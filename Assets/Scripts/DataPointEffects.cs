ec02c9678df0ec475c76cc97b4d821cc602afba9
Anders Markendahl <anders.markendahl@ericsson.com>
2022-04-11 22:17:35 +0200

    Align color of effect

--- Assets/Scripts/DataPointEffects.cs
+++ Assets/Scripts/DataPointEffects.cs
@@ -7,16 +7,19 @@ public class DataPointEffects : MonoBehaviour
     private Vector3 _positionNorm;
     private Renderer _renderer;
     private Color _color;
-
+    private ParticleSystem _ps;
     void Awake()
     {
         _renderer = GetComponent<Renderer>();
         _positionNorm = transform.position.normalized;
         _color = new Color(_positionNorm.x, _positionNorm.y, _positionNorm.z, 1f);
-    }
+        _ps = GetComponentInChildren<ParticleSystem>();
 
+    }
     void Start()
     {         
+        var main = _ps.main;
+        main.startColor = _color;
         _renderer.material.color = _color;
     }
 }
