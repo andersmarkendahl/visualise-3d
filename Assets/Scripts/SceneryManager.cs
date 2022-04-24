5b7e4423036c8af8b91f8f783013e011d0a8e3ef
Anders Markendahl <anders.markendahl@ericsson.com>
2022-04-24 21:19:03 +0200

    Fadout/FadeIn at Intro scene

---
+++ Assets/Scripts/SceneryManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+public class SceneryManager : MonoBehaviour
+{
+    public static SceneryManager Instance;
+
+    private Animator _animator;
+    private float _transitionDelay = 2.0f;
+
+    private IEnumerator DelayLoadLevel(string level)
+    {
+        _animator.SetTrigger("TriggerTransition");
+        yield return new WaitForSeconds(_transitionDelay);
+        SceneManager.LoadScene(level);
+    }
+    public void LoadLevel(string level)
+    {
+        StartCoroutine(DelayLoadLevel(level));
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    void Awake()
+    {
+        Instance = this;
+        GameObject transitionImage = GameObject.Find("TransitionImage");
+        if (transitionImage == null)
+        {
+            Debug.LogError("Unable to find transitionImage");
+        }
+        else
+        {
+            _animator = transitionImage.GetComponent<Animator>();
+            if (_animator == null)
+                Debug.LogError("Unable to get _animator");
+        }
+    }
+}
