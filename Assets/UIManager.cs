84148e478ba5f299c87f4900a412eacfc3dfabcd
Anders Markendahl <anders.markendahl@ericsson.com>
2022-04-25 21:04:19 +0200

    Using SimpleFileBrowser instead of EditorUtils

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,16 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using SimpleFileBrowser;
+
 public class UIManager : MonoBehaviour
 {
     public void LoadFile()
     {
-        string path = EditorUtility.OpenFilePanel("Open data file", "", "json");
-        Debug.Log(path);
-        SceneryManager.Instance.LoadLevel("Run");
+        StartCoroutine(ShowLoadDialogCoroutine());
     }
     public void Quit()
     {
         Application.Quit();
+    }
+    IEnumerator ShowLoadDialogCoroutine()
+    {
+        yield return FileBrowser.WaitForLoadDialog( FileBrowser.PickMode.Files, false, null, null, "Load JSON Configuration", "Load" );
+        if(FileBrowser.Success)
+        {
+            Debug.Log(FileBrowser.Result[0]);
+            SceneryManager.Instance.LoadLevel("Run");
+        }
+        else
+        {
+            Debug.LogError("Failed to open file");
+        }
+    }
+    void Start()
+    {
+        FileBrowser.SetFilters(true, new FileBrowser.Filter( "json", ".json"));
+        FileBrowser.SetDefaultFilter( ".json" );
     }
 }
