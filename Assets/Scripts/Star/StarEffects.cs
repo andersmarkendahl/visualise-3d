cceda70b120ffccd221a38318dbcb6605a44a12e
Anders Markendahl <anders.markendahl@ericsson.com>
2022-05-08 13:46:32 +0200

    Rotate stars (fixed #14)

--- Assets/Scripts/Star/StarEffects.cs
+++ Assets/Scripts/Star/StarEffects.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class StarEffects : MonoBehaviour
 {
     private Vector3 _positionNorm;
+    private Vector3 _rotationAxis = Vector3.up;
+    private float _rotationSpeed = 10.0f;
     private Renderer _renderer;
     private Color _color;
     private ParticleSystem _ps;
@@ -21,5 +23,10 @@ public class StarEffects : MonoBehaviour
         var main = _ps.main;
         main.startColor = _color;
         _renderer.material.color = _color;
+    }
+    void Update()
+    {
+        // Rotate star around fixed axis
+		transform.RotateAround(transform.position, _rotationAxis, _rotationSpeed * Time.deltaTime);
     }
 }
