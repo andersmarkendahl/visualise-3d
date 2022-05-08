cc5b7e6df6815aab63f6fd0d6380927f14b460e3
Anders Markendahl <anders.markendahl@ericsson.com>
2022-05-08 22:14:17 +0200

    FadeIn Placeholder panel on Star (#9)

--- Assets/Scripts/Star/StarInfoManager.cs
+++ Assets/Scripts/Star/StarInfoManager.cs
@@ -9,22 +9,44 @@ public class StarInfoManager : MonoBehaviour
 
     public DataPoint Dpoint { get => _dpoint; set => _dpoint = value; }
     public int Id { get => _id; set => _id = value; }
-    public float FadeTime = 2.0f;
+    public float HeaderFadeTime, PanelFadeTime;
     public TMP_Text Header;
+    public Image PanelBackground;
+    public TMP_Text PanelName, PanelText;
 
     private float _origHeaderAlpha;
     private DataPoint _dpoint;
     private int _id;
 
+    private IEnumerator PanelFade(float targetAlpha)
+    {
+        var elapsedTime = 0.0f;
+        var startABackground = PanelBackground.color.a;
+        var startAPanelName = PanelName.color.a;
+        var startAPanelText = PanelText.color.a;
+
+        // Gradually Fade Panel
+        while (elapsedTime < PanelFadeTime)
+        {
+            float alpha1 = Mathf.Lerp(startABackground, targetAlpha, (elapsedTime / PanelFadeTime));
+            float alpha2 = Mathf.Lerp(startAPanelName, targetAlpha, (elapsedTime / PanelFadeTime));
+            float alpha3 = Mathf.Lerp(startAPanelText, targetAlpha, (elapsedTime / PanelFadeTime));
+            PanelBackground.color = new Color(PanelBackground.color.r, PanelBackground.color.g, PanelBackground.color.b, alpha1);
+            PanelName.color = new Color(PanelName.color.r, PanelName.color.g, PanelName.color.b, alpha2);
+            PanelText.color = new Color(PanelText.color.r, PanelText.color.g, PanelText.color.b, alpha3);
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
     private IEnumerator HeaderFade(float targetAlpha)
     {
         var elapsedTime = 0.0f;
-        var startAlpha = Header.color.a;
+        var startA = Header.color.a;
 
         // Gradually Fade Labels
-        while (elapsedTime < FadeTime)
+        while (elapsedTime < HeaderFadeTime)
         {
-            float alpha = Mathf.Lerp(startAlpha, targetAlpha, (elapsedTime / FadeTime));
+            float alpha = Mathf.Lerp(startA, targetAlpha, (elapsedTime / HeaderFadeTime));
             Header.color = new Color(Header.color.r, Header.color.g, Header.color.b, alpha);
             elapsedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
@@ -33,6 +55,8 @@ public class StarInfoManager : MonoBehaviour
     public void ButtonClicked() {
         UserZoom.Instance.StarClicked(this);
         StartCoroutine(HeaderFade(0.0f));
+        StartCoroutine(PanelFade(1.0f));
+
     }
     void Start()
     {
